748ea7f910db8d420a8c6a52d4865c6a6977e58a
Matthew Walters (s3780012) <s3780012@student.rmit.edu.au>
2020-05-19 16:50:33 +1000

    Generated User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # Static views
-  get '/home', to: 'static_pages#home'
-  get '/about', to: 'static_pages#about'
+  get '/home',    to: 'static_pages#home'
+  get '/about',   to: 'static_pages#about'
   # User Views
+  get '/signup',  to: 'users#new'
 end
