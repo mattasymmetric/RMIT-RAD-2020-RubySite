b36b084e140ae5dcb54a8b003e457458bf8430fd
Matthew Walters (s3780012) <s3780012@student.rmit.edu.au>
2020-05-19 12:10:26 +1000

    Added title building for views based on online tutorial.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup 
+    @title = "| News Plus"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
+    assert_select "title", "Home#{@title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About#{@title}"
   end
 
 end
