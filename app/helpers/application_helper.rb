0d7d4ddc16312b58b6b02a3ab2963ed0341a6ad4
Matthew Walters (s3780012) <s3780012@student.rmit.edu.au>
2020-05-19 16:14:31 +1000

    Added dynamic title, Added and styled header and footer.
    Additionally changed the gitignore to ignore vscode settings
    Applied significant site wide styling, to aid development.
    Nav links still require to have implemented testing.
    Added SVG file for a user that isnt logged in.
    The header is currently static as no models have been implemented.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  # [1, pg. 178]
+  def page_title(current_page = '')
+    title = "News Plus"
+    if current_page.empty?
+      title
+    else
+      current_page + " | " + title
+    end
+  end
+
+  def navlink_class(path)
+    if current_page?(path)
+      "active"
+    else
+      ""
+    end
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,20 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  # [1, pg. 150-151]
   def setup 
-    @title = "| News Plus"
+    @base_title = "News Plus"
   end
 
   test "should get home" do
     get home_path
     assert_response :success
-    assert_select "title", "Home#{@title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About#{@title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
