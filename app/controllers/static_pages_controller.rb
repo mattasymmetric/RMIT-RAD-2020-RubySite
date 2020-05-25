22684ff84e4a7f218bbf107c104ac8c62b2c47ae
Matthew Walters (s3780012) <s3780012@student.rmit.edu.au>
2020-05-25 15:42:42 +1000

    Continued working on home page style and view.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
-  def home
+  include SessionsHelper
 
+  def home
+    temp_user = current_user
+    @temp_uesrname = "FUUUUCk " + temp_user.displayname
   end
 
   def about
